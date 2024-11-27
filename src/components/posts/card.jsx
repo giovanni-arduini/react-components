2bb581341b9c76ac6942ea17be6d7f61684fd038
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-27 16:20:37 +0100

    ADD Card.module.css

--- src/components/posts/card.jsx
+++ src/components/posts/card.jsx
@@ -1,15 +1,16 @@
 import image from "../../assets/images.png";
 import Button from "../UI/button/button";
+import style from "./Card.module.css";
 
 export default function card() {
   return (
-    <div className="post-card">
+    <div className={style.card}>
       <div className="card-image">
-        <img className="post-thumb" src={image} alt="" />
+        <img className={style.thumbnail} src={image} alt="" />
       </div>
-      <div className="card-body">
-        <h3 className="post-title">Titolo del post</h3>
-        <div className="post-description">
+      <div className={style.body}>
+        <h3 className={style.title}>Titolo del post</h3>
+        <div className={style.description}>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda
           omnis illum doloremque porro sit veniam exercitationem autem animi ab
           eligendi?
