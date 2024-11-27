f51f9a09bf211d25d832f42b4f99682e4cd579ce
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-27 15:49:16 +0100

    FIX header positioning and spacing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ export default function Footer() {
   return (
     <footer className="page-footer">
       <section className="container">
-        <div>Qui c'è il footer!</div>
+        <div>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut
+          similique voluptate esse veniam labore fugiat possimus optio
+          perferendis delectus suscipit.
+        </div>
       </section>
     </footer>
   );
