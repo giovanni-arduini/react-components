2aa02053e1bc0a26bb9c0d2af39bb3242c4a836e
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-27 15:29:55 +0100

    ADD Card to Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,14 @@
+import Card from "./posts/card";
+
 export default function Main() {
   return (
     <main className="page-main">
       <section>
         <div className="container">
           <div className="row">
-            <div className="col-6">Cards here!</div>
+            <div className="col-6">
+              <Card />
+            </div>
           </div>
         </div>
       </section>
