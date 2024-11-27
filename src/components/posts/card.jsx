e9c36f21df2bd251e9bd30b1956019840bb909ba
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-27 15:22:35 +0100

    ADD card component

---
+++ src/components/posts/card.jsx
@@ -0,0 +1,20 @@
+import image from "../../assets/images.png";
+
+export default function card() {
+  return (
+    <div className="post-card">
+      <div className="card-image">
+        <img className="post-thumb" src={image} alt="" />
+      </div>
+      <div className="card-body">
+        <h3 className="post-title">Titolo del post</h3>
+        <div className="post-description">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda
+          omnis illum doloremque porro sit veniam exercitationem autem animi ab
+          eligendi?
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
